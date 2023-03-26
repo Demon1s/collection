d1e6177fd82d33967e8de34f6a3c954f78b96b6a
Evgeniy Skuratov <streetdem0n@yandex.ru>
2023-03-26 18:39:00 +0300

    add method insert

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
         ArrayListOfInts arrays = new ArrayListOfInts();
         arrays.add(1);
         arrays.add(2);
-        arrays.insert(1,111);
+        arrays.insert(0,111);
+        arrays.insert(2,222);
 
 
         System.out.println(arrays.toStrings());
